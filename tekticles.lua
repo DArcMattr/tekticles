100764fd38906dc181f12a7af9614c9df3063e36
David Arceneaux <david@davidthemachine.org>
2012-01-03 02:09:29 -0800

    remove all trace of Calibri fonts

--- tekticles.lua
+++ tekticles.lua
@@ -85,13 +85,7 @@ f:SetScript("OnEvent", function()
     f:SetFont(NORMAL, size, "OUTLINE")
   end
 
-  for _,butt in pairs(PaperDollTitlesPane.buttons) do butt.text:SetFontObject(GameFontHighlightSmallLeft) end
+  for _, butt in pairs(PaperDollTitlesPane.buttons) do 
+    butt.text:SetFontObject(GameFontHighlightSmallLeft) 
+  end
 end)
-
-local LSM = LibStub and LibStub:GetLibrary("LibSharedMedia-3.0", true)
-if LSM then
-  LSM:Register("font", "Calibri", [[Interface\Addons\SharedMedia_MyMedia\font\Calibri.ttf]])
-  LSM:Register("font", "Calibri Bold", [[Interface\Addons\SharedMedia_MyMedia\font\CalibriBold.ttf]])
-  LSM:Register("font", "Calibri Bold Italic", [[Interface\Addons\SharedMedia_MyMedia\font\CalibriBoldItalic.ttf]])
-  LSM:Register("font", "Calibri Italic", [[Interface\Addons\SharedMedia_MyMedia\font\CalibriItalic.ttf]])
-end
